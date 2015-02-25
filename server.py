d7b1b6880121a245d85d28f9918033d8ffdfb083
Patrick G <patrickguffeyUS@gmail.com>
2015-02-24 20:05:34 -0600

    updated naming, added files to gitignore

--- server.py
+++ server.py
@@ -3,77 +3,68 @@ from datetime import datetime
 import calendar
 from pymongo import Connection
 import bson
-from json import dumps
 
 client = Connection('localhost', port=27017)
 db = client.todo
 
-@get('/test/')
-def test():
-    return '{"success": "true"}'
 
 @post('/create/')
 def create_post():
-	new_post = {}
-	post_data = request.forms.dict
-	new_post['title'] = post_data['title']
-	new_post['desc'] = post_data['desc']
-	new_post['is_deleted'] = '0'
-	new_post['created_date'] = calendar.timegm(datetime.utctimetuple(datetime.now()))
-	new_post['updated_date'] = new_post['created_date']
-	new_post['is_completed'] = '0'
+    new_post = {}
+    post_data = request.forms.dict
+    new_post['title'] = post_data['title']
+    new_post['desc'] = post_data['desc']
+    new_post['is_deleted'] = '0'
+    new_post['created_date'] = calendar.timegm(datetime.utctimetuple(datetime.now()))
+    new_post['updated_date'] = new_post['created_date']
+    new_post['is_completed'] = '0'
 
-	db.post.insert(new_post)
+    db.post.insert(new_post)
 
-	new_post['_id'] = str(new_post['_id'])
+    new_post['_id'] = str(new_post['_id'])
 
-	return new_post
+    return new_post
 
 
 @post('/delete/')
 def delete_post():
-	post = request.forms.dict
-	post = db['post'].find_one({'_id':bson.ObjectId(post['_id'])})
-	post['is_deleted'] = '1'
-	db.post.save(post)
-	post['_id'] = str(post['_id'])
+    post_data = request.forms.dict
+    post_to_delete = db['post'].find_one({'_id': bson.ObjectId(post_data['_id'])})
+    post_to_delete['is_deleted'] = '1'
+    db.post.save(post_to_delete)
+    post_to_delete['_id'] = str(post_to_delete['_id'])
 
-	return '{"deleted": "true"}'
+    return '{"deleted": "true"}'
 
 
 @post('/edit_post/')
 def edit_post():
-	post = request.forms.dict
-	post['_id'] = bson.ObjectId(post['_id'])
-	db.post.update({'_id': post['_id']},{'$set':post})
-	post['_id'] = str(post['_id'])
-	return post
+    post = request.forms.dict
+    post['_id'] = bson.ObjectId(post['_id'])
+    db.post.update({'_id': post['_id']}, {'$set': post})
+    post['_id'] = str(post['_id'])
+    return post
 
-@post ('/set_completed/')
+
+@post('/set_completed/')
 def set_completed(post_id):
-	post_data = request.forms.dict
-	post = db['post'].find_one({'_id':bson.ObjectId(post_id)})
-	post['is_completed'] = post_data['is_completed']
-	db.post.save(post)
-	post['_id'] = str(post['_id'])
-	return post
+    post_data = request.forms.dict
+    completed_post = db['post'].find_one({'_id': bson.ObjectId(post_id)})
+    completed_post['is_completed'] = post_data['is_completed']
+    db.post.save(completed_post)
+    completed_post['_id'] = str(completed_post['_id'])
+    return completed_post
 
 
 @get('/get_post/:post_id')
 def get_post(post_id):
-	post = db['post'].find_one({'_id':bson.ObjectId(post_id)})
-	post['_id'] = str(post['_id'])
+    found_post = db['post'].find_one({'_id': bson.ObjectId(post_id)})
+    found_post['_id'] = str(found_post['_id'])
 
-	if post['is_deleted'] == '0' or post['is_deleted'] == 0:
-		return post  
-	else: 
-		return {}
+    if found_post['is_deleted'] == '0' or found_post['is_deleted'] == 0:
+        return found_post
+    else:
+        return '{"error": "no post found with given ID"}'
 
-@get('/get_first/')
-def get_a_post():
-	post = db['post'].find_one()
-	post ['_id'] = str(post['_id'])
-	return post
-	
-run (host='0.0.0.0', port=4567)
 
+run(host='0.0.0.0', port=4567)
